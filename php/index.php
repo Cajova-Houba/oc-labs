e6dd5e8a47d8bcc3088b36f2ec5ef908eae4e990
Zdenek Vales <zdenek.vales@yoso.fi>
2020-08-03 16:35:22 +0200

    Simple PHP app.

---
+++ php/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <title>PHP OC test</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+</head>
+
+<body>
+  <?php
+    phpinfo();
+  ?>
+</body>
+
+</html>
---
+++ php/readme.md
@@ -0,0 +1,3 @@
+# About
+
+One-page php web application to test creating a new OC application from the source code.
